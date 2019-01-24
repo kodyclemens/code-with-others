917e7c24e2910e5a5af96658de5349e28503c22c
kodyclemens <kody.clemens@gmail.com>
2019-01-24 14:50:40 -0500

    omniauth for Github setup and working. Controller is a huge mess and will be refactored.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,7 +13,7 @@ class SessionsController < ApplicationController
         flash[:errors] = user.errors.full_messages
         render "users/new"
       end
-    else
+    elsif params[:username]
       @user = User.find_by(username: params[:username])
       if @user.nil?
         redirect_to login_path
@@ -24,6 +24,16 @@ class SessionsController < ApplicationController
         else
           redirect_to login_path
         end
+      end
+    elsif auth
+      @user = User.find_by(github_uid: auth[:uid])
+      if @user.nil?
+        @user = User.create(github_uid: auth[:uid], username: auth[:info][:nickname], email: auth[:info][:email], avatar_url: auth[:info][:image], github_profile_url: auth[:info][:urls][:GitHub], password: SecureRandom.hex, bio: auth[:extra][:raw_info][:bio], company: auth[:extra][:raw_info][:company])
+        session[:user_id] = @user.id
+        redirect_to root_path
+      else
+        session[:user_id] = @user.id
+        redirect_to root_path
       end
     end
   end
@@ -37,5 +47,9 @@ class SessionsController < ApplicationController
 
   def session_params
     params.require(:session).permit(:username, :real_name, :email, :password, :password_confirmation, :github_uid, :github_profile_url, :company, :bio, :avatar_url, :communication_method)
+  end
+
+  def auth
+    request.env['omniauth.auth']
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'welcome#index'
   get '/login', to: 'sessions#new'
+  get '/auth/github/callback', to: 'sessions#create'
   get '/signup', to: 'users#new'
   delete '/logout', to: 'sessions#destroy'
   resources :sessions, only: [:create]
