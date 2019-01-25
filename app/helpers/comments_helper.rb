6cae9aeb29e3d8bde402aebf307fa4f1fef18bc1
kodyclemens <kody.clemens@gmail.com>
2019-01-25 18:46:53 -0500

    comment user and date timestamp added to team show page. comment helper added to format date timestamp.

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,2 +1,10 @@
 module CommentsHelper
+  def username(user_id)
+    user = User.find(user_id)
+    user.username
+  end
+
+  def formatted_datetime(date_timestamp)
+    date_timestamp.strftime("%A, %B %e, %Y at %l:%M %p")
+  end
 end
