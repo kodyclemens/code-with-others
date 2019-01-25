3611695d1a614f061229740244f28fcf8ad03789
kodyclemens <kody.clemens@gmail.com>
2019-01-25 16:36:21 -0500

    add view helper to see if user is logged in. flash errors and AR validation errors seperated into their own partial

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,17 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
     if params[:session]
-      user = User.new(user_params)
-      if user.save
+      @user = User.new(user_params)
+      if @user.valid?
+        @user.save
         session[:user_id] = user.id
         redirect_to root_path
       else
-        render "users/new"
+        render 'users/new'
       end
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root 'welcome#index'
-  get '/login', to: 'sessions#new'
+  get '/login', to: 'sessions#new', as: 'login'
   get '/auth/github/callback', to: 'sessions#create'
-  get '/signup', to: 'users#new'
+  get '/signup', to: 'users#new', as: 'signup'
   delete '/logout', to: 'sessions#destroy'
   resources :sessions, only: [:create]
   resources :users, except: [:new]
