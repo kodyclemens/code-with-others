964ba5930f8bb6e099220db4ab093a763e964108
kodyclemens <kody.clemens@gmail.com>
2019-01-25 21:44:38 -0500

    comment index, new, and create added

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,39 @@
 class CommentsController < ApplicationController
+  before_action :require_login
+  before_action :set_user
+  before_action :set_team
+
+  def index
+    @comments = Comment.where(team_id: @team.id)
+  end
+
+  def new
+    @comment = Comment.new
+  end
+
+  def create
+    @comment = Comment.new(comment_params)
+    @comment.team_id = @team.id
+    @comment.user_id = @user.id
+    if @comment.valid?
+      @comment.save
+      redirect_to team_path(@team)
+    else
+      render 'comments/new'
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content, :user_id, :team_id)
+  end
+
+  def set_user
+    @user = User.find(session[:user_id])
+  end
+
+  def set_team
+    @team = Team.find(params[:team_id])
+  end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,8 @@
 class Comment < ApplicationRecord
+  default_scope { order(updated_at: :desc) }
+  validates :content, presence: true
+  validates :user_id, presence: true
+  validates :team_id, presence: true
   belongs_to :team
   belongs_to :user
 end
