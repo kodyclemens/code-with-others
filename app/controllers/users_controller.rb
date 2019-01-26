ee204202060a9b0c01fa4c54a1c57a01522c1558
kodyclemens <kody.clemens@gmail.com>
2019-01-25 22:06:39 -0500

    beginning user profile show, edit and update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,19 @@
 class UsersController < ApplicationController
+  before_action :require_login, except: [:new, :create]
+  before_action :set_user, only: [:edit, :show, :update]
+
   def new
     @user = User.new
+  end
+
+  def show
+    
+  end
+
+  def edit
+  end
+
+  def update
   end
 
   def create
@@ -20,5 +33,9 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:session).permit(:username, :real_name, :email, :password, :password_confirmation, :github_uid, :github_profile_url, :company, :bio, :avatar_url, :communication_method)
+  end
+
+  def set_user
+    @user = User.find(session[:user_id])
   end
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -1,9 +1,9 @@
 class Team < ApplicationRecord
+  validates :name, presence: true
+  validates :creator_id, presence: true
   has_many :user_teams
   has_many :users, through: :user_teams
   has_many :comments
   has_many :team_technologies
   has_many :technologies, through: :team_technologies
-  validates :name, presence: true
-  validates :creator_id, presence: true
 end
