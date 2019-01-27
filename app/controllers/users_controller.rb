dbe5c4f7f465b724a8c6505fcc197ddb5c57ec58
kodyclemens <kody.clemens@gmail.com>
2019-01-27 17:36:18 -0500

    add error display to recaptcha

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -35,7 +35,7 @@ class UsersController < ApplicationController
   def create
     if params[:user]
       @user = User.new(user_params)
-      if @user.valid? && verify_recaptcha(model: @user)
+      if verify_recaptcha(model: @user) && @user.valid? 
         @user.save
         session[:user_id] = @user.id
         redirect_to root_path
