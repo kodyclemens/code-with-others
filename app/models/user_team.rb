9839255df4c5d6e1214abf72b170fd9479b2ce08
kodyclemens <kody.clemens@gmail.com>
2019-01-28 15:36:48 -0500

    prevent users from joining a team more than once

--- app/models/user_team.rb
+++ app/models/user_team.rb
@@ -1,5 +1,6 @@
 class UserTeam < ApplicationRecord
   validates :user_id, presence: true
+  validates_uniqueness_of :user_id, scope: :team_id, message: 'You are already in this group!'
   validates :team_id, presence: true
 
   belongs_to :user
