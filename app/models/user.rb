962922413ff7ac2cf5fc554337f8b19c004462f3
kodyclemens <kody.clemens@gmail.com>
2019-03-25 20:30:58 -0400

    testing a bug when user signs up via Github

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
   validates :username, presence: true, uniqueness: true
-  validates :email, presence: true, uniqueness: true
+  validates :email, uniqueness: true
   validates :password, presence: true, confirmation: true, length: { minimum: 1 }, allow_nil: true
   validates :github_uid, uniqueness: true
 
